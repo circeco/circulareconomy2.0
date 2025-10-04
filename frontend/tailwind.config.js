b18de46eac3790c1c054828aa3222c323d476e92
circeco <pg.softwaredeveloper@gmail.com>
2025-10-04 15:13:16 +0200

    angular extention version bug

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
+    // require('@tailwindcss/line-clamp'),
   ],
 };
